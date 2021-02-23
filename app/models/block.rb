4dbae92cafcb314c7d6a853a52673e9580f8955d
antonlukashevich <luka14a@gmail.com>
2021-02-23 23:59:26 +0300

    blocks relationship too

--- app/models/block.rb
+++ app/models/block.rb
@@ -1,3 +1,3 @@
 class Block < ApplicationRecord
-	#belongs_to :course
+	belongs_to :course
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ApplicationRecord
-	#has_many :blocks
+	has_many :blocks
 
 	has_many :lectures
 end
